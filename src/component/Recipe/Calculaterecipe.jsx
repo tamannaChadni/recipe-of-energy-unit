39765fef19fc0dc55c9dbd751359378eb293f9cb
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-07-02 17:00:35 +0600

    editing

---
+++ src/component/Recipe/Calculaterecipe.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const Calculaterecipe = (props) => {
+const Calculaterecipe = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+Calculaterecipe.propTypes = {};
+
+export default Calculaterecipe;
---
+++ src/component/Recipe/Recipedetails.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// const Recipedetails = props => {
+const Recipedetails = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+Recipedetails.propTypes = {
+    
+};
+
+export default Recipedetails;
